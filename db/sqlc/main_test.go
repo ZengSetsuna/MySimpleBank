3fb52d17cc4124ba80b69f15b0354ac071c20414
ZengSetsuna <gvayyz@gmail.com>
2024-11-14 08:47:31 +0000

    Initial commit

---
+++ db/sqlc/main_test.go
@@ -0,0 +1,35 @@
+package db
+
+import (
+	"context"
+	"log"
+	"os"
+	"testing"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	_ "github.com/lib/pq"
+)
+
+// var testQueries *Queries
+// var testDB *sql.DB
+var testStore Store
+
+const (
+	dbDriver = "postgres"
+	dbSource = "postgresql://shu:shu@localhost:5432/simple_bank?sslmode=disable"
+)
+
+func TestMain(m *testing.M) {
+	//config, err := util.LoadConfig("../..")
+	//if err != nil {
+	//	log.Fatal("cannot load config:", err)
+	//}
+
+	connPool, err := pgxpool.New(context.Background(), dbSource)
+	if err != nil {
+		log.Fatal("cannot connect to db:", err)
+	}
+
+	testStore = NewStore(connPool)
+	os.Exit(m.Run())
+}
---
+++ util/random.go
@@ -0,0 +1,37 @@
+package util
+
+import (
+	"math/rand"
+	"time"
+)
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func RandomInt(min, max int64) int64 {
+	return min + rand.Int63n(max-min+1)
+}
+
+func RandomString(n int64) string {
+	const letters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = letters[rand.Int63n(int64(len(letters)))]
+	}
+	return string(b)
+}
+
+func RandomOwner() string {
+	return RandomString(6)
+}
+
+func RandomMoney() int64 {
+	return RandomInt(0, 1000)
+}
+
+func RandomCurrency() string {
+	currencies := []string{"USD", "EUR"}
+	n := len(currencies)
+	return currencies[rand.Intn(n)]
+}
